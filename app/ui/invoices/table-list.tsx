06ddd52d4bf9f6f11d75ea557705be815b5adb8c
marcelines <marcio@status.im>
2023-12-12 14:02:19 +0000

    feat: adds possiblity to check multiple items on table

---
+++ app/ui/invoices/table-list.tsx
@@ -0,0 +1,184 @@
+'use client';
+
+import { ChangeEvent, MouseEvent, useState } from 'react';
+import Image from 'next/image';
+import { UpdateInvoice, DeleteInvoice } from '@/app/ui/invoices/buttons';
+import InvoiceStatus from '@/app/ui/invoices/status';
+import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
+import { Invoice } from './table';
+import clsx from 'clsx';
+
+export const InvoicesList = ({ invoices }: { invoices: Invoice[] }) => {
+  const [selected, setSelected] = useState<string[]>([]);
+  const [firstSelected, setFirstSelected] = useState<string | null>(null);
+
+  const handleSelect = (id: string, event: MouseEvent<HTMLInputElement>) => {
+    let newSelected = [...selected];
+
+    if (event.shiftKey && firstSelected) {
+      const start = invoices.findIndex(
+        (invoice) => invoice.id === firstSelected,
+      );
+      const end = invoices.findIndex((invoice) => invoice.id === id);
+      const ids = invoices
+        .slice(Math.min(start, end), Math.max(start, end) + 1)
+        .map((invoice) => invoice.id);
+      newSelected = ids;
+    } else {
+      if (newSelected.includes(id)) {
+        newSelected = newSelected.filter((item) => item !== id);
+      } else {
+        newSelected = [...newSelected, id];
+        setFirstSelected(id); // Set the first selected item
+      }
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleSelectAll = () => {
+    if (selected.length === invoices.length) {
+      setSelected([]);
+    } else {
+      setSelected(invoices.map((invoice) => invoice.id));
+    }
+  };
+
+  const isSelected = (id: string) => selected.includes(id);
+
+  console.log(selected);
+
+  return (
+    <div className="mt-6 flow-root">
+      <div className="inline-block min-w-full align-middle">
+        <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
+          <div className="md:hidden">
+            {invoices?.map((invoice) => (
+              <div
+                key={invoice.id}
+                className="mb-2 w-full rounded-md bg-white p-4"
+              >
+                <div className="flex items-center justify-between border-b pb-4">
+                  <div>
+                    <div className="mb-2 flex items-center">
+                      <Image
+                        src={invoice.image_url}
+                        className="mr-2 rounded-full"
+                        width={28}
+                        height={28}
+                        alt={`${invoice.name}'s profile picture`}
+                      />
+                      <p>{invoice.name}</p>
+                    </div>
+                    <p className="text-sm text-gray-500">{invoice.email}</p>
+                  </div>
+                  <InvoiceStatus status={invoice.status} />
+                </div>
+                <div className="flex w-full items-center justify-between pt-4">
+                  <div>
+                    <p className="text-xl font-medium">
+                      {formatCurrency(invoice.amount)}
+                    </p>
+                    <p>{formatDateToLocal(invoice.date)}</p>
+                  </div>
+                  <div className="flex justify-end gap-2">
+                    <UpdateInvoice id={invoice.id} />
+                    <DeleteInvoice id={invoice.id} />
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <table className="hidden min-w-full text-gray-900 md:table">
+            <thead className="rounded-lg text-left text-sm font-normal">
+              <tr>
+                <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
+                  Customer
+                </th>
+                <th scope="col" className="px-3 py-5 font-medium">
+                  Email
+                </th>
+                <th scope="col" className="px-3 py-5 font-medium">
+                  Amount
+                </th>
+                <th scope="col" className="px-3 py-5 font-medium">
+                  Date
+                </th>
+                <th scope="col" className="px-3 py-5 font-medium">
+                  Status
+                </th>
+                <th scope="col" className="relative py-3 pl-6 pr-3">
+                  <span className="sr-only">Edit</span>
+                </th>
+                <th scope="col" className="relative py-3 pl-6 pr-3">
+                  <div className="flex items-center justify-end">
+                    <input
+                      id="default-checkbox"
+                      type="checkbox"
+                      className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+                      onChange={handleSelectAll}
+                      checked={selected.length === invoices.length}
+                    />
+                  </div>
+                </th>
+              </tr>
+            </thead>
+            <tbody className="bg-white">
+              {invoices?.map((invoice) => (
+                <tr
+                  key={invoice.id}
+                  className={clsx([
+                    'w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg',
+                    isSelected(invoice.id) && 'bg-blue-50',
+                  ])}
+                >
+                  <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                    <div className="flex items-center gap-3">
+                      <Image
+                        src={invoice.image_url}
+                        className="rounded-full"
+                        width={28}
+                        height={28}
+                        alt={`${invoice.name}'s profile picture`}
+                      />
+                      <p>{invoice.name}</p>
+                    </div>
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-3">
+                    {invoice.email}
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-3">
+                    {formatCurrency(invoice.amount)}
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-3">
+                    {formatDateToLocal(invoice.date)}
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-3">
+                    <InvoiceStatus status={invoice.status} />
+                  </td>
+                  <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                    <div className="flex justify-end gap-3">
+                      <UpdateInvoice id={invoice.id} />
+                      <DeleteInvoice id={invoice.id} />
+                    </div>
+                  </td>
+                  <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                    <div className="flex items-center justify-end">
+                      <input
+                        id="default-checkbox"
+                        type="checkbox"
+                        className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+                        onClick={(event) => handleSelect(invoice.id, event)}
+                        checked={isSelected(invoice.id)}
+                      />
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
